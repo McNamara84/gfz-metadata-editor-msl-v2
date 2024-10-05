3efbc710034f956ccb9229c633762e499be897cb
McNamara <holgerehrmann@gmail.com>
2024-10-05 07:58:19 +0200

    SaveAuthorsTest erstellt
    testSaveSingleAuthorWithAllFields testet das Speichern eines einzelnen Autors mit allen Feldern befüllt, testSaveThreeAuthorsWithAllFields testet das Speichern von drei Autoren mit allen Feldern ausgefüllt, testSaveSingleAuthorWithOnlyRequiredFields testet das Speichern eines einzelnen Autors befüllt nur mit den Pflichtfeldern, testSaveAuthorWithEmptyFields testet das Verhalten beim Versuch, einen Autor mit komplett leeren Feldern zu speichern, testSaveThreeAuthorsWithOneMissingLastName testet das Speichern von drei Autoren, von denen einer einen fehlenden Nachnamen (Pflichtfeld) hat, testSaveAuthorsWithMultipleAffiliations testet das Speichern von Autoren mit mehreren Affiliationen, testSaveAuthorsWithMixedAffiliationsAndRorIds testet das Speichern von Autoren mit gemischten Affiliationen und ROR-IDs, testSpecialCasesAndEdgeConditions testet unterschiedliche Spezialfälle beim Speichern von Autoren

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e19537a6d2dc68f705e65b7cbc65f9823bbac953',
+        'reference' => '550cd9529479c5d3ad431400ebf7a1e34ce24ac2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e19537a6d2dc68f705e65b7cbc65f9823bbac953',
+            'reference' => '550cd9529479c5d3ad431400ebf7a1e34ce24ac2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
