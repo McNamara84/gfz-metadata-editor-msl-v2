243a742fd527871d9de1f3e4d14b7991093d1b84
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-10-16 21:29:11 +0200

    Unit-Tests aktualisiert/ fixed #91
    Die Unit-Tests für Thesaurus-Keywords wurden umgeschrieben. Die alten Felder wurden entfernt und durch das neue ersetzt.

--- tests/SaveThesauruskeywordsTest.php
+++ tests/SaveThesauruskeywordsTest.php
@@ -56,31 +56,7 @@ class SaveThesauruskeywordsTest extends TestCase
 
         $postData = [
             "thesaurusKeywords" => json_encode([["value" => "Keyword1", "id" => "http://example.com/1", "scheme" => "CustomScheme", "schemeURI" => "http://example.com/scheme", "language" => "en"]]),
-            "GENMaterial" => json_encode([["value" => "Material1"]]),
-            "GENGeologicalAge" => json_encode([["value" => "Age1", "id" => "http://example.com/age1"]]),
-            "GENPoreFluid" => json_encode([["value" => "Fluid1"]]),
-            "GENGeologicalSetting" => json_encode([["value" => "Setting1"]]),
-            "AMOGPModeledStructure" => json_encode([["value" => "Structure1"]]),
-            "AMOGPModeledGeomorphologicalFeature" => json_encode([["value" => "Feature1"]]),
-            "AMOGPApparatus" => json_encode([["value" => "Apparatus1"]]),
-            "AMOGPAncillaryEquipment" => json_encode([["value" => "Equipment1"]]),
-            "AMOGPAMOGPMeasuredProperty" => json_encode([["value" => "Property1"]]),
-            "AMOGPSoftware" => json_encode([["value" => "Software1"]]),
-            "GEOCHAnalysisTechnique" => json_encode([["value" => "Technique1"]]),
-            "GEOCHMeasuredProperty" => json_encode([["value" => "Property2"]]),
-            "MATomographyApparatus" => json_encode([["value" => "Apparatus2"]]),
-            "MATomographyAncillaryEquipment" => json_encode([["value" => "Equipment2"]]),
-            "MATomographyAnalysisTechnique" => json_encode([["value" => "Technique2"]]),
-            "MATomographyAnalyzedFeature" => json_encode([["value" => "Feature2"]]),
-            "AMOGPMATomographyInferredParameter" => json_encode([["value" => "Parameter1"]]),
-            "PALTISApparatus" => json_encode([["value" => "Apparatus3"]]),
-            "PALTISEnvironmentControl" => json_encode([["value" => "Control1"]]),
-            "PALTISMeasuredProperty" => json_encode([["value" => "Property3"]]),
-            "PALTISInferredBehavior" => json_encode([["value" => "Behavior1"]]),
-            "RAMPApparatus" => json_encode([["value" => "Apparatus4"]]),
-            "RAMPAncillaryEquipment" => json_encode([["value" => "Equipment3"]]),
-            "RAMPMeasuredProperty" => json_encode([["value" => "Property4"]]),
-            "RAMPInferredDeformationBehavior" => json_encode([["value" => "Behavior2"]])
+            "MSLKeywords" => json_encode([["value" => "Material1"]]),
         ];
 
         saveThesaurusKeywords($this->connection, $postData, $resource_id);
@@ -90,7 +66,7 @@ class SaveThesauruskeywordsTest extends TestCase
         $stmt->execute();
         $result = $stmt->get_result();
 
-        $this->assertEquals(26, $result->num_rows, "Es sollten genau 26 Thesaurus Keywords gespeichert worden sein.");
+        $this->assertEquals(2, $result->num_rows, "Es sollten genau 26 Thesaurus Keywords gespeichert worden sein.");
 
         $expectedKeywords = [
             "Keyword1", "Material1", "Age1", "Fluid1", "Setting1", "Structure1", "Feature1", "Apparatus1", "Equipment1", 
@@ -136,80 +112,8 @@ class SaveThesauruskeywordsTest extends TestCase
             "thesaurusKeywords" => json_encode([
                 ["value" => "Keyword1"], ["value" => "Keyword2"], ["value" => "Keyword3"]
             ]),
-            "GENMaterial" => json_encode([
+            "MSLKeywords" => json_encode([
                 ["value" => "Material1"], ["value" => "Material2"], ["value" => "Material3"]
-            ]),
-            "GENGeologicalAge" => json_encode([
-                ["value" => "Age1"], ["value" => "Age2"], ["value" => "Age3"]
-            ]),
-            "GENPoreFluid" => json_encode([
-                ["value" => "Fluid1"], ["value" => "Fluid2"], ["value" => "Fluid3"]
-            ]),
-            "GENGeologicalSetting" => json_encode([
-                ["value" => "Setting1"], ["value" => "Setting2"], ["value" => "Setting3"]
-            ]),
-            "AMOGPModeledStructure" => json_encode([
-                ["value" => "Structure1"], ["value" => "Structure2"], ["value" => "Structure3"]
-            ]),
-            "AMOGPModeledGeomorphologicalFeature" => json_encode([
-                ["value" => "Feature1"], ["value" => "Feature2"], ["value" => "Feature3"]
-            ]),
-            "AMOGPApparatus" => json_encode([
-                ["value" => "Apparatus1"], ["value" => "Apparatus2"], ["value" => "Apparatus3"]
-            ]),
-            "AMOGPAncillaryEquipment" => json_encode([
-                ["value" => "Equipment1"], ["value" => "Equipment2"], ["value" => "Equipment3"]
-            ]),
-            "AMOGPAMOGPMeasuredProperty" => json_encode([
-                ["value" => "Property1"], ["value" => "Property2"], ["value" => "Property3"]
-            ]),
-            "AMOGPSoftware" => json_encode([
-                ["value" => "Software1"], ["value" => "Software2"], ["value" => "Software3"]
-            ]),
-            "GEOCHAnalysisTechnique" => json_encode([
-                ["value" => "Technique1"], ["value" => "Technique2"], ["value" => "Technique3"]
-            ]),
-            "GEOCHMeasuredProperty" => json_encode([
-                ["value" => "Property4"], ["value" => "Property5"], ["value" => "Property6"]
-            ]),
-            "MATomographyApparatus" => json_encode([
-                ["value" => "Apparatus4"], ["value" => "Apparatus5"], ["value" => "Apparatus6"]
-            ]),
-            "MATomographyAncillaryEquipment" => json_encode([
-                ["value" => "Equipment4"], ["value" => "Equipment5"], ["value" => "Equipment6"]
-            ]),
-            "MATomographyAnalysisTechnique" => json_encode([
-                ["value" => "Technique4"], ["value" => "Technique5"], ["value" => "Technique6"]
-            ]),
-            "MATomographyAnalyzedFeature" => json_encode([
-                ["value" => "Feature4"], ["value" => "Feature5"], ["value" => "Feature6"]
-            ]),
-            "AMOGPMATomographyInferredParameter" => json_encode([
-                ["value" => "Parameter1"], ["value" => "Parameter2"], ["value" => "Parameter3"]
-            ]),
-            "PALTISApparatus" => json_encode([
-                ["value" => "Apparatus7"], ["value" => "Apparatus8"], ["value" => "Apparatus9"]
-            ]),
-            "PALTISEnvironmentControl" => json_encode([
-                ["value" => "Control1"], ["value" => "Control2"], ["value" => "Control3"]
-            ]),
-            "PALTISMeasuredProperty" => json_encode([
-                ["value" => "Property7"], ["value" => "Property8"], ["value" => "Property9"]
-            ]),
-            "PALTISInferredBehavior" => json_encode([
-                ["value" => "Behavior1"], ["value" => "Behavior2"], ["value" => "Behavior3"]
-            ]),
-            "RAMPApparatus" => json_encode([
-                ["value" => "Apparatus10"], ["value" => "Apparatus11"], ["value" => "Apparatus12"]
-            ]),
-            "RAMPAncillaryEquipment" => json_encode([
-                ["value" => "Equipment7"], ["value" => "Equipment8"], ["value" => "Equipment9"]
-            ]),
-            "RAMPMeasuredProperty" => json_encode([
-                ["value" => "Property10"], ["value" => "Property11"], ["value" => "Property12"]
-            ]),
-            "RAMPInferredDeformationBehavior" => json_encode([
-                ["value" => "Behavior4"], ["value" => "Behavior5"], ["value" => "Behavior6"]
             ])
         ];
 
@@ -220,7 +124,7 @@ class SaveThesauruskeywordsTest extends TestCase
         $stmt->execute();
         $result = $stmt->get_result()->fetch_assoc();
 
-        $this->assertEquals(78, $result['count'], "Es sollten genau 78 Thesaurus Keywords gespeichert worden sein.");
+        $this->assertEquals(6, $result['count'], "Es sollten genau 78 Thesaurus Keywords gespeichert worden sein.");
 
         // Check if all keywords are linked to the resource
         $stmt = $this->connection->prepare("SELECT COUNT(*) as count FROM Resource_has_Thesaurus_Keywords WHERE Resource_resource_id = ?");
@@ -228,7 +132,7 @@ class SaveThesauruskeywordsTest extends TestCase
         $stmt->execute();
         $result = $stmt->get_result()->fetch_assoc();
 
-        $this->assertEquals(78, $result['count'], "Es sollten genau 78 Verknüpfungen zwischen Resource und Thesaurus Keywords existieren.");
+        $this->assertEquals(6, $result['count'], "Es sollten genau 78 Verknüpfungen zwischen Resource und Thesaurus Keywords existieren.");
     }
 
     /**
@@ -251,12 +155,8 @@ class SaveThesauruskeywordsTest extends TestCase
         $postData = [
             "thesaurusKeywords" => json_encode([["value" => "Keyword1"]]),
             "GENMaterial" => "",
-            "GENGeologicalAge" => json_encode([["value" => "Age1"]]),
+            "MSLKeywords" => json_encode([["value" => "Age1"]]),
             "GENPoreFluid" => "",
-            "GENGeologicalSetting" => json_encode([["value" => "Setting1"]]),
-            "AMOGPModeledStructure" => "",
-            "AMOGPModeledGeomorphologicalFeature" => "",
-            "AMOGPApparatus" => json_encode([["value" => "Apparatus1"]]),
             "AMOGPAncillaryEquipment" => "",
             "AMOGPAMOGPMeasuredProperty" => "",
             "AMOGPSoftware" => "",
@@ -284,7 +184,7 @@ class SaveThesauruskeywordsTest extends TestCase
         $stmt->execute();
         $result = $stmt->get_result();
 
-        $this->assertEquals(4, $result->num_rows, "Es sollten genau vier Thesaurus Keywords gespeichert worden sein.");
+        $this->assertEquals(2, $result->num_rows, "Es sollten genau vier Thesaurus Keywords gespeichert worden sein.");
 
         $keywords = [];
         while ($row = $result->fetch_assoc()) {
@@ -293,8 +193,8 @@ class SaveThesauruskeywordsTest extends TestCase
 
         $this->assertContains("Keyword1", $keywords, "Keyword1 sollte gespeichert worden sein.");
         $this->assertContains("Age1", $keywords, "Age1 sollte gespeichert worden sein.");
-        $this->assertContains("Setting1", $keywords, "Setting1 sollte gespeichert worden sein.");
-        $this->assertContains("Apparatus1", $keywords, "Apparatus1 sollte gespeichert worden sein.");
+        $this->assertContains("Age1", $keywords, "Setting1 sollte gespeichert worden sein.");
+        $this->assertContains("Keyword1", $keywords, "Apparatus1 sollte gespeichert worden sein.");
         $this->assertNotContains("Material1", $keywords, "Material1 sollte nicht gespeichert worden sein.");
     }
 
