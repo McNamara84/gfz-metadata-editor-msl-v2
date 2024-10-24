65e95e75c583ed00e196340ac8502cabc31ec626
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-10-24 12:40:21 +0200

    #11
    -Die Funktion (checkMandatoryFields) wurde um Eingabefelder von Contributing Institution(s) erweitert.
    
    -Die Kommentare wurden auf Englisch übersetzt.

--- js/checkMandatoryFields.js
+++ js/checkMandatoryFields.js
@@ -1,18 +1,33 @@
 function checkMandatoryFields() {
+    // First name and last name fields (Contributing Person(s))
     var firstname = document.getElementById('inputContributorFirstname').value.trim();
     var lastname = document.getElementById('inputContributorLastname').value.trim();
 
-    // Role and ORCID fields
+    // Role and ORCID fields (Contributing Person(s))
     var roleField = document.getElementById('inputContributorsPerRole');
     var orcidField = document.getElementById('inputContributorORCID');
 
+    // Organization name and role fields (Contributing Institution(s))
+    var OrganisationName = document.getElementById('inputOrganisationName').value.trim();
+    var institutionRoleField = document.getElementById('inputContributorOrgaRole');
+
+    // Check if first name or last name(Contributing Person(s)) is filled
     if (firstname !== '' || lastname !== '') {
-        // Wenn Vorname oder Nachname eingegeben wurde, mache Role und ORCID erforderlich
+        // Set 'required' for role and ORCID fields
         roleField.setAttribute('required', 'required');
         orcidField.setAttribute('required', 'required');
     } else {
-        // Wenn beide leer sind, entferne die Erforderlichkeit von Role und ORCID
+        // Remove 'required' from role and ORCID fields
         roleField.removeAttribute('required');
         orcidField.removeAttribute('required');
+    }
+
+    // Check if organization name(Contributing Institution(s)) is filled
+    if (OrganisationName !== '') {
+        // Set 'required' for institution role field
+        institutionRoleField.setAttribute('required', 'required');
+    } else {
+        // Remove 'required' from institution role field
+        institutionRoleField.removeAttribute('required');
     }
 }
