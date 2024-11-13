680c434a03a6b44430d439bc74b0373fc3f56744
McNamara <holgerehrmann@gmail.com>
2024-11-13 14:36:28 +0100

    Code-Dokumentation nach PHPDoc

--- doc/help.php
+++ doc/help.php
@@ -1,3 +1,12 @@
 <?php
+/**
+ *
+ * This script includes the settings and displays the help documentation.
+ *
+ */
+
+// Include the settings.php file to access configuration variables
 include_once "../settings.php";
+
+// Include the help.html file to display the help content
 include "help.html";
