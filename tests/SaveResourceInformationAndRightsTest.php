a87a0ca00b998920b8da26034093f50db1fdbccd
McNamara <holgerehrmann@gmail.com>
2024-11-14 14:44:05 +0100

    DB-Name im Test zurückgesetzt auf Ausgangszustand

--- tests/SaveResourceInformationAndRightsTest.php
+++ tests/SaveResourceInformationAndRightsTest.php
@@ -29,7 +29,7 @@ class SaveResourceInformationAndRightsTest extends TestCase
         }
         $this->connection = $connection;
         // Überprüfen, ob die Testdatenbank verfügbar ist
-        $dbname = 'geobrains-msl-test';
+        $dbname = 'mde2-msl-test';
         if ($this->connection->select_db($dbname) === false) {
             // Testdatenbank erstellen
             $connection->query("CREATE DATABASE " . $dbname);
