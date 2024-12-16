2cae6c53362fd3c93e0844c8e2a069d79b3bc491
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-12-16 10:10:06 +0100

    fixed #231
    - Validierung der Eingabefelder in "FG" Contact Person(s) überarbeitet: Wenn eines der Felder ausgefüllt ist, werden lastname, fistname und e-mail address zu Pflichtfeldern.
    - Kommentare im Contributors-Bereich aktualisiert.

--- js/checkMandatoryFields.js
+++ js/checkMandatoryFields.js
@@ -1,30 +1,49 @@
 function checkMandatoryFields() {
-    // First name and last name fields (Contributing Person(s))
+    // "FG" Contributer(s)
+    //Person
     var firstname = document.getElementById('input-contributor-firstname').value.trim();
     var lastname = document.getElementById('input-contributor-lastname').value.trim();
-
-    // Role and ORCID fields (Contributing Person(s))
     var roleField = document.getElementById('input-contributor-personrole');
-
-    // Organization name and role fields (Contributing Institution(s))
+    //Institution
     var OrganisationName = document.getElementById('input-contributor-name').value.trim();
     var institutionRoleField = document.getElementById('input-contributor-organisationrole');
 
-    // Check if first name or last name(Contributing Person(s)) is filled
+    // Check if first name or last name is filled
     if (firstname !== '' || lastname !== '') {
-        // Set 'required' for role and ORCID fields
+        // Set 'required' for role field
         roleField.setAttribute('required', 'required');
     } else {
-        // Remove 'required' from role and ORCID fields
+        // Remove 'required' from role field
         roleField.removeAttribute('required');
     }
 
-    // Check if organization name(Contributing Institution(s)) is filled
+    // Check if organization name is filled
     if (OrganisationName !== '') {
         // Set 'required' for institution role field
         institutionRoleField.setAttribute('required', 'required');
     } else {
         // Remove 'required' from institution role field
         institutionRoleField.removeAttribute('required');
+    }
+
+    // "FG" Contact person(s)
+    var contactpersonLastname = document.getElementById('input-contactperson-lastname');
+    var contactpersonFirstname = document.getElementById('input-contactperson-firstname');
+    var contactpersonPosition = document.getElementById('input-contactperson-position');
+    var contactpersonEmail = document.getElementById('input-contactperson-email');
+    var contactpersonWebsite = document.getElementById('input-contactperson-website');
+    var contactpersonAffiliation = document.getElementById('input-contactperson-affiliation');
+
+    // Check if any contact person fields are filled
+    if (contactpersonLastname.value.trim() !== '' || contactpersonFirstname.value.trim() !== '' || contactpersonPosition.value.trim() !== '' || contactpersonEmail.value.trim() !== '' || contactpersonWebsite.value.trim() !== '' || contactpersonAffiliation.value.trim() !== '') {
+        // Set 'required' for Contributors Lastname, Contact Person Firstname and Contact Email Address fields
+        contactpersonLastname.setAttribute('required', 'required');
+        contactpersonFirstname.setAttribute('required', 'required');
+        contactpersonEmail.setAttribute('required', 'required');
+    } else {
+        // Remove 'required' from Contributors Lastname, Contact Person Firstname and Contact Email Address fields
+        contactpersonLastname.removeAttribute('required');
+        contactpersonFirstname.removeAttribute('required');
+        contactpersonEmail.removeAttribute('required');
     }
 }
